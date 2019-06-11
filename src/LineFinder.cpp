7d889310ca87f9bee526e4b6420aa60a1a098d3b
Kevin ANDRIEUX <kevin.andrieux@viacesi.fr>
2019-06-11 14:11:07 +0200

    reformat

--- src/LineFinder.cpp
+++ src/LineFinder.cpp
@@ -2,50 +2,58 @@
 
 const int signalPinExtremLeft = 2;
 const int signalPinLeft = 4;
-const int signalPinRight = 9; 
+const int signalPinRight = 9;
 const int signalPinExtremRight = 6;
 
-LineFinder::LineFinder(){ 
-  
-  Serial.begin(9600);
+LineFinder::LineFinder()
+{
 
-pinMode(signalPinExtremLeft, INPUT);
-pinMode(signalPinLeft, INPUT);
-pinMode(signalPinRight, INPUT);
-pinMode(signalPinExtremRight, INPUT);
+    Serial.begin(9600);
 
+    pinMode(signalPinExtremLeft, INPUT);
+    pinMode(signalPinLeft, INPUT);
+    pinMode(signalPinRight, INPUT);
+    pinMode(signalPinExtremRight, INPUT);
 }
 
-ECatchLine LineFinder::find(void){
+ECatchLine LineFinder::find(void)
+{
 
-    if( !( boolRead(signalPinExtremLeft) || boolRead(signalPinExtremRight) )  && boolRead(signalPinLeft)  && boolRead(signalPinRight)){
-        Serial.println("No correction");   
+    if (!(boolRead(signalPinExtremLeft) || boolRead(signalPinExtremRight)) && boolRead(signalPinLeft) && boolRead(signalPinRight))
+    {
+        Serial.println("No correction");
         return (Straight);
     }
-    if( boolRead(signalPinRight) && !(boolRead(signalPinExtremLeft) || boolRead(signalPinLeft)  || boolRead(signalPinExtremRight) ) ){
-        Serial.println("Little right correction");   
+    if (boolRead(signalPinRight) && !(boolRead(signalPinExtremLeft) || boolRead(signalPinLeft) || boolRead(signalPinExtremRight)))
+    {
+        Serial.println("Little right correction");
         return (Right);
     }
-    if( boolRead(signalPinLeft) && !( boolRead(signalPinExtremLeft) || boolRead(signalPinRight) || boolRead(signalPinExtremRight) ) ){
-        Serial.println("Little left correction");   
+    if (boolRead(signalPinLeft) && !(boolRead(signalPinExtremLeft) || boolRead(signalPinRight) || boolRead(signalPinExtremRight)))
+    {
+        Serial.println("Little left correction");
         return (Left);
     }
-    if( boolRead(signalPinExtremRight) ){
-        Serial.println("Big right correction");   
+    if (boolRead(signalPinExtremRight))
+    {
+        Serial.println("Big right correction");
         return (ExtremeRight);
     }
-    if( boolRead(signalPinExtremLeft) && !boolRead(signalPinExtremRight) ){
-        Serial.println("Big left correction");   
+    if (boolRead(signalPinExtremLeft) && !boolRead(signalPinExtremRight))
+    {
+        Serial.println("Big left correction");
         return (ExtremeLeft);
     }
-     if( !(  boolRead(signalPinExtremLeft)   && boolRead(signalPinLeft)  && boolRead(signalPinRight) && boolRead(signalPinExtremRight) ) ){
-        Serial.println("Reverse");   
+    if (!(boolRead(signalPinExtremLeft) && boolRead(signalPinLeft) && boolRead(signalPinRight) && boolRead(signalPinExtremRight)))
+    {
+        Serial.println("Reverse");
         return (Reverse);
     }
-return (Error);
+    return (Error);
 }
 
-boolean LineFinder::boolRead(int signalPin){
+boolean LineFinder::boolRead(int signalPin)
+{
     if (HIGH == digitalRead(signalPin))
         return true;
 
