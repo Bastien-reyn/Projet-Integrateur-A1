d17309fc6d303e500a9c0392a1fa1fc3a5e76531
Valentin <v.zonzon@live.fr>
2019-06-19 16:45:32 +0200

    add modes for reciever and sender

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define RECIEVE
 #include <Arduino.h>
 #include "Robot.h"
 #include "Map.h"
@@ -5,30 +6,43 @@
 #include "RadioReciever.h"
 #include "RadioSender.h"
 
+
 unsigned long time = 0;
 
 Robot *robot;
 LineFinder* lineFinder;
 MotorSpeedSensor* motorSpeedSensor;
 Map *theMap;
+#ifdef RECIEVE
 RadioReciever *radioreciever;
+#endif
+#ifdef SEND
 RadioSender *radiosender;
+#endif
 
 // La fonction setup de l'Arduino
 void setup()
 {
-   Serial.begin(9600);
-radioreciever = new RadioReciever();
+  Serial.begin(9600);
+  #ifdef RECIEVE
+  radioreciever = new RadioReciever();
+  #endif
+  #ifdef SEND
   radiosender = new RadioSender();
+  #endif
   Serial.print("init");
 }
 
 // La loop de l'Arduino
 void loop()
 { 
+  #ifdef SEND
   String test = "coucou";
   radiosender->Send(test.c_str(),test.length());
-  //radioreciever->Recieve();
+  #endif
+  #ifdef RECIEVE
+  radioreciever->Recieve();
+  #endif
   delay(100);
   Serial.print(".");
 
