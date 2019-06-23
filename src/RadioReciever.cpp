9e26c206b22c6f8d5289036ae2abc237d40440c8
Valentin <v.zonzon@live.fr>
2019-06-23 20:26:21 +0200

    Test double authentification

--- src/RadioReciever.cpp
+++ src/RadioReciever.cpp
@@ -13,10 +13,13 @@ vw_rx_start();
 
 void RadioReciever::Recieve()
 {
+    // tableau qui va stocker le message recu
     uint8_t buf[80];
+    // Variable qui va contenir la longueur du message (par défaut, le max = 80)
     uint8_t buflen = 80;
+    // La clé de décryptage AES
     uint8_t key[] = {0x76, 0x61, 0x6c, 0x65, 0x73, 0x74, 0x75, 0x6e, 0x63, 0x6f, 0x6e, 0x6e, 0x61, 0x72, 0x64, 0x21}; // La clé de cryptage AES
-    if (vw_get_message(buf, &buflen)) //Renvoie "true" si le message reçu est correct
+    if (vw_get_message(buf, &buflen) /*&& buf[0]=='A' en cas de double authentification*/) //Renvoie "true" si le message reçu est correct
     {
         int i;
         Serial.print("Recu: ");
@@ -26,6 +29,22 @@ void RadioReciever::Recieve()
           Serial.print(buf[i]);
           Serial.print(" ");
         }
+        ////////////////////// DOUBLE AUTHENTIFICATION /////////////////////
+        /*
+        uint8_t temp=0;
+        for (int i = 1; i < buflen ; i++)
+        {
+          if (buf[i]!=0)
+          {
+            temp=buf[i];
+            buf[i-1]=temp;
+          }
+          else
+          {
+            buf[i-1]=0;
+          }
+        }
+         */
         aes128_dec_single(key, buf);
         if ((char)buf[0]=='A')
         {
--- src/RadioSender.cpp
+++ src/RadioSender.cpp
@@ -17,9 +17,11 @@ void RadioSender::send(String Donnees)
     //On copie dans le paquet le message
     //strcpy(package, Message);
 
-    //On convertit crypte le message en AES
-    int taillemsg= Donnees.length() + 1;
+    //Taille message + 1 pour la clé
+    int taillemsg= Donnees.length() + 1;  // +2 si double authentification
+    //On convertit la taille en fonction du nombre de données en héxadécimal (16 bits) à envoyer 
     taillemsg=(taillemsg%16) ? ((taillemsg/16)+1)*16 : taillemsg;
+    //On convertit les données String en const char pour l'envoi et le cryptage
     const char *Message=Donnees.c_str();
     uint8_t key[] = {118, 97, 108, 101, 115, 116, 117, 110, 99, 111, 110, 110, 97, 114, 100, 33}; // La clé de cryptage AES
     uint8_t* mts = (uint8_t*) malloc((taillemsg) * sizeof(uint8_t)); //On crée le tableau de la taille du nombre de caractères du message +1 (pour la clé)
@@ -30,29 +32,41 @@ void RadioSender::send(String Donnees)
         mts[i] = 0;
     }
     //On écrit le message sur le tableau à envoyer
-    mts[0] = 'A'; //La clé d'identification
+    mts[0] = 'A'; //La clé d'identification cryptée
     Serial.print((char)mts[0]);
-    for(int i = 1; i < taillemsg; i++)
+    //On ajoute dans les données à envoyer le message en question
+    for(int i = 1; i < taillemsg; i++)     
     {
         mts[i] = uint8_t(Message[i-1]);
         Serial.print((char)mts[i]);
     }
     aes128_enc_single(key, mts); //On appelle la fonction qui va encoder le message en fonction de la clé fournie
+
+
+    //////////////////////////// DOUBLE AUTHENTIFICATION ///////////////////////////////
+    /*
+    const char temp=0;
+    for (int i = 0; i < taillemsg; i++)
+    {
+        temp=mts[i];
+        mts[i+1]=temp;
+    }
+    mts[0]='A';
+     */
+
+
     #ifdef DEBUG
     Serial.println("données cryptées");
     //Serial.println(String(Message));
-    #endif
-
     for(int i = 0; i < taillemsg; i++)
     {
         Serial.print(mts[i], HEX);
         Serial.print(" ");
 
     }
-    //On commence l'envoi du paquet / message
-    //vw_send(paquet, 20);
-    
     Serial.print(taillemsg);
+    #endif
+
     vw_send(mts, taillemsg);
 
     //Cette fonction va bloquer le programme jusqu'a ce que l'envoi soit bien terminé 
